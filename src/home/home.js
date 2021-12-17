1efef7e957a22605000ed3143b1072d67521fbba
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-17 00:21:45 -0500

    homepage done

--- src/home/home.js
+++ src/home/home.js
@@ -1,16 +1,9 @@
 import React, {useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap"
-import "../App.css";
-import homeBg from "../assets/homeBackground.png";
-import blueBg from "../assets/blueBackground.png";
-
 
 
 const HomePage =() =>{
-  // const pathBg = useState(window.location.pathname);
-  // const imageUrl = pathBg == "" || "/" ? homeBg : blueBg;
-  
   return (
     
     <div className="homePg">
