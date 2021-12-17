178ff893385f993fdc098bcc91df6ae2ed33d5c6
Darah Snider <33365227+darahdesigner@users.noreply.github.com>
2021-12-16 19:31:10 -0500

    initial push to new heroku app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ChoreIndex from "./chores/ChoreIndex";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ChoreCreate from "./chores/ChoreCreate";
 import ChoreTable from "./chores/ChoreTable";
-import Footer from "./home/Footer";
+// import Footer from "./home/Footer";
 import HomePage from "./home/home";
 import ContactPage from "./Contact/contactWill";
 import Footer from "./Elements/footer"
